291b9cf830cd01b977af40b3c9268a88ec3a8b42
SandiipAnish <sandiipanish@gmail.com>
2026-01-07 08:27:05 +0530

    [UPDATE] App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { FormProvider } from "./context/FormContext"
 import FormBuilder from "./components/builder/FormBuilder"
+import DynamicForm from "./components/preview/DynamicForm"
 
 const App = () => {
   return (
     <FormProvider>
       <FormBuilder />
+      <hr />
+      <DynamicForm />
     </FormProvider>
   )
 }
